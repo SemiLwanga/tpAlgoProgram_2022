668a5de149f3bf916212fd768ef5ee320b01c4bb
SEMIKENKE Deogratias <120032594+SemiLwanga@users.noreply.github.com>
2023-01-05 10:49:24 +0100

    suppression du fichier 2.

--- question2.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Entrer un nombre : "))
-def factorial(n):
-    if n==0:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-print(f"Le factoriel de {n} vaut: {factorial(n)}")
